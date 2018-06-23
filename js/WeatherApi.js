49763656c867dbf4578e51919017e4210101e1fe
Richard Keast <richardkeast@gmail.com>
2018-06-23 14:38:46 +0100

    Added updated FetchData page, which now uses OpenWeatherApi to fetch a forecast for your chosen city. (Not quite working yet)

---
+++ js/WeatherApi.js
@@ -0,0 +1,17 @@
+﻿Blazor.registerFunction('jsonP', function (url) {
+
+    var data = {};
+    var callback = function (data) {
+        const apiCallback = Blazor.platform.findMethod(
+            'CornishCoderBlazor',
+            'CornishCoderBlazor.Pages',
+            'FetchData',
+            'ApiCallback'
+        );
+        
+        console.log(data);
+        Blazor.platform.callMethod(apiCallback, null, [Blazor.platform.toDotNetString(JSON.stringify(data))]);
+    };
+
+    J50Npi.getJSON(url, data, callback);
+});
