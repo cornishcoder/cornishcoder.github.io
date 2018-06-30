745f57e7cce218789dc667a54eaefdcbd4d9b57d
Richard Keast <richardkeast@gmail.com>
2018-06-30 12:21:52 +0100

    OpenWeatherMap Api now works

---
+++ js/jsonP.js
@@ -0,0 +1,24 @@
+﻿Blazor.registerFunction('jsonP', function (url) {
+    return new Promise(function (resolve, reject) {
+        var id = '_' + Math.round(10000 * Math.random());
+        var callbackName = 'jsonp_callback_' + id;
+
+        window[callbackName] = function (data) {
+            delete window[callbackName];
+            var ele = document.getElementById(id);
+            ele.parentNode.removeChild(ele);
+            var jsonAsString = JSON.stringify(data);
+            resolve(jsonAsString);
+        };
+
+        // remove 2 double quotes from url string passed from .Net
+        url = url.replace('"', '');
+        url = url.replace('"', '');
+        var src = url + '&callback=' + callbackName;
+        var script = document.createElement('script');
+        script.src = src;
+        script.id = id;
+        script.addEventListener('error', reject);
+        (document.getElementsByTagName('head')[0] || document.body || document.documentElement).appendChild(script)
+    });
+});
---
+++ md/home.md
@@ -0,0 +1,8 @@
+﻿## Hello!
+
+# markdown ftw
+
+Well this works...
+
+| Col 1 | Col 2 |
+| Hello | There |
